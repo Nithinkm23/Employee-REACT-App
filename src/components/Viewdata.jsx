a209bcb89c6dccd8b9f6820447fc5bb0a4c76ad4
NithinKM23 <nithinkm55@gmail.com>
2023-07-10 13:22:48 +0530

    Update Viewdata.jsx

--- src/components/Viewdata.jsx
+++ src/components/Viewdata.jsx
@@ -6,12 +6,12 @@ const Viewdata = () => {
 
     const [data, setData] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response)=>{
-            setData(response.data)
-        })
-        .catch(err=>console.log(err))
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch(err => console.log(err))
     })
 
 
@@ -20,33 +20,33 @@ const Viewdata = () => {
 
 
     return (
-        
+
         <div className="container App">
             <div className="row">
-            <p class="fw-light fs-4">EMPLOYEE DATA</p>
+                <p class="fw-light fs-4">EMPLOYEE DATA</p>
                 <div className="col col-12 col-sm-12 col-md-12">
 
                     <div className="row g-3">
-                            {data.map((val,i)=>{
+                        {data.map((val, i) => {
 
-                          
-                         return <div className="col col-12 col-sm-6 col-md-6 col-lg-6">
-                            {/* //card starts */}
-                            <div class="card" >
-                                <div class="card-header">
-                                    Employee Details
+
+                            return <div className="col col-12 col-sm-6 col-md-6 col-lg-6">
+                                {/* //card starts */}
+                                <div class="card" >
+                                    <div class="card-header">
+                                        Employee Details
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item"><p>Id :  {val.id}</p></li>
+                                        <li class="list-group-item"><p>Name :   {val.name}</p></li>
+                                        <li class="list-group-item"><p>Email  :  {val.email}</p></li>
+                                    </ul>
                                 </div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item"><p>Id :  {val.id}</p></li>
-                                    <li class="list-group-item"><p>Name :   {val.name}</p></li>
-                                    <li class="list-group-item"><p>Email  :  {val.email}</p></li>
-                                </ul>
-                            </div>
 
-                        </div>
-                         }
-                                
-                         )}
+                            </div>
+                        }
+
+                        )}
 
 
 
